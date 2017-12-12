d50f0cf510bfb1516f4dd628866e4dd428ee69aa
Sudip Saha <sudipstudy39@gmail.com>
2017-12-12 13:18:48 +0530

    updated html,css and js file

--- colorgame.js
+++ colorgame.js
@@ -1,17 +1,39 @@
 var colors=generateRandom(6);
+
 	
 var square=document.querySelectorAll(".square");
 var rgb=document.querySelector("#RGB");
 var pickColor=pickrandom();
+var result=document.querySelector("#result");
+var h1=document.querySelector("h1");
+var reset=document.querySelector("#newgame");
+reset.addEventListener("click",function(){
+	colors=generateRandom(6);
+	reset.textContent="New Game";
+	pickColor=pickrandom();
+	rgb.textContent=pickColor;
+	for(var i=0;i<colors.length;i++)
+		square[i].style.background=colors[i];
+	h1.style.background="black";
+});
 rgb.textContent=pickColor;
 for(var i=0;i<square.length;i++)
 {
 	square[i].style.background=colors[i];
 	square[i].addEventListener("click",function(){
 		if(this.style.background===pickColor)
-			alert("correct");
+		{
+			result.textContent="Correct";
+			h1.style.background=pickColor;
+			for(i=0;i<6;i++)
+				square[i].style.background=pickColor;
+			reset.textContent="playAgain!";
+		}
 		else
-			alert("wrong");
+		{
+			result.textContent="Wrong";
+			this.style.background="black";
+		}
 
 	});
 
